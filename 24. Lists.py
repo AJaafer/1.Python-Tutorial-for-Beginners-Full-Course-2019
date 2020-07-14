9d3b6e85a747f4b4ae87ec2c41a2b07d36a0e7da
Lusala2015 <you@example.com>
2020-07-14 09:57:51 +0300

    displays largest number in a list

--- 24. Lists.py
+++ 24. Lists.py
@@ -9,3 +9,12 @@ print(names[2:])
 
 names = ["mark" , "bob" , "mosh", "sarah", "marry"]
 print(names[2:4])
+
+numbers =[3,5,7,9,0,4,2]
+max = numbers [0]
+for number in numbers :
+    if number>max:
+        max=number
+print(max)
+
+
