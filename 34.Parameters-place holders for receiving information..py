98c81fe33a81f639b16f180721336ac279439f3d
Lusala2015 <you@example.com>
2020-07-14 10:26:36 +0300

    great advantage of functions

--- 34.Parameters-place holders for receiving information..py
+++ 34.Parameters-place holders for receiving information..py
@@ -5,3 +5,12 @@ def greet_user(name):
 print("Start")
 greet_user("john")
 print("finish")
+
+def greet_user(name):
+    print(f"Hi {name} !")
+    print("Welcome abroad !!")
+
+print("Start")
+greet_user("john")
+greet_user("mary")
+print("finish")
