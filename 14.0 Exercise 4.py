c2cc27459e3ddff4500e81c2de3c599a4a3a6c38
Lusala2015 <you@example.com>
2020-07-14 09:29:11 +0300

    exercise

---
+++ 14.0 Exercise 4.py
@@ -0,0 +1,6 @@
+#Imagin the price of a house is $1M
+#                 If the buyer has good credit
+#                              They need to put down 10%
+#                 Otherwise
+#                                  they need to put down 20%
+# write down a program to display a buyer with good credit
