d44acf666a4901b5f75b9c17d86ed3fd4fcc7b09
Lusala2015 <you@example.com>
2020-07-14 10:18:22 +0300

    Adds a name in the dictionaries

--- 30. Dictionaries.py
+++ 30. Dictionaries.py
@@ -11,3 +11,12 @@ customer ={
     "is verified": True
 }
 print(customer.get("dirth", "i am doing great"))
+
+customer ={
+    "name":"john Smith",
+    "age": 30,
+    "is verified": True
+}
+customer ["name"] = "sheila kiseako"
+print(customer["name"])
+
