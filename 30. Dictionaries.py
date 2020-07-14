12ac240ead9e31528a1fec5e9587cafb24926a3d
Lusala2015 <you@example.com>
2020-07-14 10:16:42 +0300

    instead of returning none it prints what is in the get ()

--- 30. Dictionaries.py
+++ 30. Dictionaries.py
@@ -4,3 +4,10 @@ customer ={
     "is verified": True
 }
 print(customer ["name"])
+
+customer ={
+    "name":"john Smith",
+    "age": 30,
+    "is verified": True
+}
+print(customer.get("dirth", "i am doing great"))
