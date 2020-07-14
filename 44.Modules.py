24fac3a2cf0cd41f4300ccca0d704b6a4f2e5c45
Lusala2015 <you@example.com>
2020-07-14 10:59:37 +0300

    app.py module

--- 44.Modules.py
+++ 44.Modules.py
@@ -1,2 +1,13 @@
 import converters
 print((converters.kg_to_lbs(50)))
+
+def lbs_to_kg(weight):
+    return weight*0.45
+def kg_to_lbs (weight):
+    return weight/0.45
+
+import converters
+from converters import kg_to_lbs
+print(kg_to_lbs(34))
+
+
