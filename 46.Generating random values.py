b7c56236819a325f81d66f3eab562dfcc4b0d179
Lusala2015 <you@example.com>
2020-07-14 11:09:03 +0300

    program that when a dice is thrown the walues changes as per the toss

--- 46.Generating random values.py
+++ 46.Generating random values.py
@@ -8,3 +8,13 @@ for i in range (4):
 members = ["mark", "lusala", "sailas","faith","sheila","kilmon"]
 pastors = random.choice(members)
 print(pastors)
+
+#write a program that when a dice is thrown the walues changes as per the toss
+import random
+class Dice:
+    def roll(self):
+        first = random.randint(1,6)
+        second = random.randint(1, 6)
+        return first, second
+dice = Dice()
+print(dice.roll())
