5fd4c148afa5df9fbbae4c0dde09d3849bf3f78f
Lusala2015 <you@example.com>
2020-07-14 09:29:43 +0300

    solution

--- 14.0 Exercise 4.py
+++ 14.0 Exercise 4.py
@@ -4,3 +4,11 @@
 #                 Otherwise
 #                                  they need to put down 20%
 # write down a program to display a buyer with good credit
+
+price = 1000000
+has_good_credit = True
+if has_good_credit :
+    down_payment = 0.1*price
+else :
+    down_payment = 0.2*price
+print(f"Down payment : ${down_payment}")
